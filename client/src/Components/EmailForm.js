8a3482c97f26a1ad10b6507f48bb848a179c092d
KarinaStabile <karina.stabile@gmail.com>
2022-12-19 09:45:53 -0500

    Commented out email-js for render

--- client/src/Components/EmailForm.js
+++ client/src/Components/EmailForm.js
@@ -1,23 +1,24 @@
 import React, { useRef } from 'react';
-import emailjs from 'emailjs-com';
+// import emailjs from 'emailjs-com';
 
 function EmailForm() {
-  const form = useRef();
+  // const form = useRef();
 
-  const sendEmail = (e) => {
-    e.preventDefault();
+  // const sendEmail = (e) => {
+  //   e.preventDefault();
  
-        emailjs.sendForm('service_au46z5s', 'template_ehnw6dw', form.current, 'M8KFb0Ew-2p9FTfkj')
-          .then((result) => {
-              console.log(result.text);
-          }, (error) => {
-              console.log(error.text);
-          });
-          e.target.reset();
-      };
+  //       emailjs.sendForm('service_au46z5s', 'template_ehnw6dw', form.current, 'M8KFb0Ew-2p9FTfkj')
+  //         .then((result) => {
+  //             console.log(result.text);
+  //         }, (error) => {
+  //             console.log(error.text);
+  //         });
+  //         e.target.reset();
+      // };
 
     return (
-        <div><form ref={form} onSubmit={sendEmail}>
+        <div>
+          {/* <form ref={form} onSubmit={sendEmail}>
         <label>Name</label>
         <input type="text" name="user_name" />
         <label>Email</label>
@@ -25,7 +26,8 @@ function EmailForm() {
         <label>Message</label>
         <textarea name="message" />
         <input type="submit" value="Send" />
-      </form></div>
+      </form> */}
+      </div>
     )
 }
 export default EmailForm;
